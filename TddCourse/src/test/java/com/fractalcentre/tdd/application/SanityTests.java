d36703b34a39de5cbe5817e30278d12b964259fd
davidputman <davidputman@davidputman.com>
2015-10-13 13:27:47 +0100

    adding first fitnesse pages

---
+++ TddCourse/src/test/java/com/fractalcentre/tdd/application/SanityTests.java
@@ -0,0 +1,14 @@
+package com.fractalcentre.tdd.application;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SanityTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
